12af28ca54bda6df59b6b4c0d1128687e69c7761
Franno <deengo113@gmail.com>
2023-11-06 14:11:19 +0100

    use stores

--- Webdock-frontend/src/main.ts
+++ Webdock-frontend/src/main.ts
@@ -3,9 +3,15 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+import adminStore from './stores/adminstore'
+import authStore from './stores/authstore'
+import searchStore from './stores/searchstore'
 
 const app = createApp(App).use(bootstrap)
 
 app.use(router)
+app.use(adminStore)
+app.use(authStore)
+app.use(searchStore)
 
 app.mount('#app')
