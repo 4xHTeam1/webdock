00c1c41c00eb382c21f1196de9fae85836265644
Jacob Højer Juhl <jacobjuhlprivat@gmail.com>
2023-11-06 11:41:05 +0100

    Opsætning af Components og Veiws folder

--- Webdock-frontend/src/Router/index.js
+++ Webdock-frontend/src/Router/index.js
@@ -7,26 +7,6 @@ const router = createRouter({
       path: '/underholdning',
       name: 'underholdning',
       component: () => import('../views/underholdning.vue')
-    },
-    {
-      path: '/synsprofil',
-      name: 'synsprofil',
-      component: () => import('../views/synsprofil.vue')
-    },
-    {
-      path: '/information',
-      name: 'information',
-      component: () => import('../views/information.vue')
-    },
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/home.vue')
-    },
-    {
-      path: '/booktid',
-      name: 'booktid',
-      component: () => import('../views/booktid.vue')
     }
   ]
 })
