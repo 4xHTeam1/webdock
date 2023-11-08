ba9ea376f034c1b36012f4d8652d170834278a81
Nikolaihoej <nhje37914@edu.ucl.dk>
2023-11-08 11:53:30 +0100

    add
    test

--- Webdock-frontend/src/Components/requestOverview.vue
+++ Webdock-frontend/src/Components/requestOverview.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="overview-area">
         <div class="feature-container">
-            <div class="feature-heading"></div>
-            <div class="feature-status"></div>
-            <div class="feature-text"></div>
+            <div class="feature-heading">bandwidth usage</div>
+            <div class="feature-status">planned</div>
+            <div class="feature-text">Can we please have an reporting tab which shows monthly bandwidth usage for a server ...</div>
         </div>
     </div>
 </template>
@@ -14,8 +14,29 @@
 
 <style>
 .overview-area{
-    background-color: black;
+    background-color: #FCFCFC;
+    border-radius: 10px;
     width: 600px;
     height: 900px;
+}
+
+.feature-container{
+    display: flex;
+    flex-direction: column;
+    padding: 18px;
+}
+
+.feature-heading{
+    font-size: 2rem;
+}
+
+.feature-status{
+    font-size: 1rem;
+    color: lightblue;
+}
+
+.feature-text{
+    font-size: 1rem;
+    text-overflow: ellipsis;
 }
 </style>
