add3d32a8217eba262d044ec59638d1789deb392
Franno <deengo113@gmail.com>
2023-11-08 09:46:08 +0100

    feature services

---
+++ Webdock-frontend/src/stores/featurestore.ts
@@ -0,0 +1,12 @@
+import { createStore } from "vuex";
+import { Authenticate } from "../services/authService";
+
+const authStore = createStore({
+  state() {
+    return {};
+  },
+  mutations: {},
+  actions: {},
+});
+
+export default authStore;
