63633088c0e6562974aca408f4b365cd9ec048aa
Mike Handberg Hovedskov <deengo113@gmail.com>
2023-11-16 17:23:15 +0100

    Added auto auth if token is in localstorage

--- Webdock-frontend/src/App.vue
+++ Webdock-frontend/src/App.vue
@@ -1,7 +1,16 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { computed } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+import { mapState, useStore } from "vuex";
+const store = useStore();
+
+//if there's a token in localstorage, dispatch the login action
+const token = localStorage.getItem("token");
+if (token) {
+  store.dispatch("auth/login", token);
+}
 </script>
 
 <template>
-<RouterView />
+  <RouterView />
 </template>
