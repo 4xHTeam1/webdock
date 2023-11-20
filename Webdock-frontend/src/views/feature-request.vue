5821b63bfad4fbea45fb8506e87c4338e7b844c8
Mike Handberg Hovedskov <deengo113@gmail.com>
2023-11-20 12:50:39 +0100

    fixed issue with feature request site

--- Webdock-frontend/src/views/feature-request.vue
+++ Webdock-frontend/src/views/feature-request.vue
@@ -1,38 +1,36 @@
 <template>
-    <navbar/>
+    <navbar />
     <div class="container d-flex justify-content-center">
-            <CreatePost/>
-            <div class="overview-area">
-                <requestOverview status="planned" color="#1FA0FF"/>
-                <requestOverview status="under review" color="#85B5B5"/>
-                <requestOverview status="in progress" color="#C17AFF"/>
-                <requestOverview status="in progress" color="#C17AFF"/>
-                <requestOverview status="in progress" color="#C17AFF"/>
-                <requestOverview status="in progress" color="#C17AFF"/>
-                <requestOverview status="in progress" color="#C17AFF"/>
-                <requestOverview status="in progress" color="#C17AFF"/>
+        <CreatePost />
+        <div class="overview-area">
+            <requestOverview status="planned" color="#1FA0FF" />
+            <requestOverview status="under review" color="#85B5B5" />
+            <requestOverview status="in progress" color="#C17AFF" />
+            <requestOverview status="in progress" color="#C17AFF" />
+            <requestOverview status="in progress" color="#C17AFF" />
+            <requestOverview status="in progress" color="#C17AFF" />
+            <requestOverview status="in progress" color="#C17AFF" />
+            <requestOverview status="in progress" color="#C17AFF" />
 
-            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import navbar from '../Components/navbar.vue'
-import CreatePost from '../Components/CreatePost.vue'
-import requestOverview from '../Components/requestOverview.vue'
 import CreatePost from '../Components/featureRequest-components/CreatePost.vue'
 import requestOverview from '../Components/featureRequest-components/requestOverview.vue'
 export default {
     components: {
-    CreatePost,
-    requestOverview,
-    navbar
-  }
+        CreatePost,
+        requestOverview,
+        navbar
+    }
 }
 </script>
 
 <style>
-body{
+body {
     width: 100%;
     height: auto;
     background-image: url("../Assets/webdock-baggrund.jpg");
@@ -40,7 +38,7 @@ body{
     background-size: cover;
 }
 
-.overview-area{
+.overview-area {
     background-color: #FCFCFC;
     border-radius: 10px;
     width: 600px;
@@ -49,6 +47,4 @@ body{
     overflow-y: auto;
     overflow-x: hidden;
 }
-
-
 </style>
