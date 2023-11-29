e806c186cd49f15a396fa0b5da0ed729d50cc42a
Mike Handberg Hovedskov <deengo113@gmail.com>
2023-11-29 16:02:12 +0100

    fixed issue with image on admin dashboard

--- Webdock-frontend/src/Components/admin/userElement.vue
+++ Webdock-frontend/src/Components/admin/userElement.vue
@@ -6,14 +6,10 @@
     <!-- userInfoContainer indeholder users firstname og lastname samtidigt med indeholder avatarURL som skal hentes fra databasen -->
     <div class="userInfoContainer">
       <div class="usersAvatar">
-        <div
-          class="noneAvatar"
-          style="background-color: #9cb"
-          v-if="user.avatarURL === null || user.avatarURL === ''"
-        >
+        <div class="noneAvatar" style="background-color: #9cb" v-if="user.avatarURL === null || user.avatarURL === ''">
           {{ user.name[0] }}
         </div>
-        <img :src="user.avatarURL" v-else />
+        <img :src="user.avatarURL" alt="Avatar" class="user_img" v-else />
       </div>
       <div class="name">{{ user.name }}</div>
     </div>
@@ -21,17 +17,10 @@
     <div class="userRole">{{ user.role }}</div>
     <!-- userSettings, er den div som har tandhjulet og skal have en dropdown menu når man klikker på svg'en dropdown menuen skal laves via script og have styling -->
     <div class="userSettings" @click="showRoleModal = true">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="22"
-        height="22"
-        fill="currentColor"
-        class="bi bi-gear-fill"
-        viewBox="0 0 16 16"
-      >
+      <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor" class="bi bi-gear-fill"
+        viewBox="0 0 16 16">
         <path
-          d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"
-        />
+          d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
       </svg>
     </div>
   </div>
@@ -77,9 +66,11 @@ export default {
 .name {
   flex: 1;
 }
+
 .user_ID {
   flex: 1;
 }
+
 .userEmail {
   flex: 1;
 }
@@ -118,5 +109,12 @@ export default {
   justify-content: space-around;
   text-transform: uppercase;
   position: absolute;
+}
+
+.user_img {
+  width: 100%;
+  height: 100%;
+  border-radius: 100px;
+  object-fit: cover;
 }
 </style>
