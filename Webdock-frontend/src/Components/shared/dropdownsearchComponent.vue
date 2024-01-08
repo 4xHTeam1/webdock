71af1671cd4c6853852205a8bc58118387aebe38
Jacob Højer Juhl <jacobjuhlprivat@gmail.com>
2024-01-08 09:19:53 +0100

    hallo

---
+++ Webdock-frontend/src/Components/shared/dropdownsearchComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="dropdownelement-container">
+        <router-link class="dropdownelement" :to="`/feature-request/${feature.id}`">{{ feature.title }}</router-link>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        feature:{
+            type: Object,
+            required: true
+        }
+    },
+    created(){
+        console.log(this.feature)
+    }
+}
+</script>
+<style>
+    .dropdownelement-container{
+        display: flex;
+        flex: 1;
+        min-width: 0;
+        max-height: 35px;
+        border-bottom: 2px solid #e0e0e0;
+    }
+    .dropdownelement-container:hover{
+        background-color: #e0e0e0;
+    }
+    .dropdownelement{
+        text-decoration: none;
+        color: #000;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+        padding: 3px;
+        z-index: 1001;
+    }
+    .dropdownelement:hover{
+        text-decoration: none;
+        color: #000;
+    }
+</style>
--- Webdock-frontend/src/Components/shared/searchComponent.vue
+++ Webdock-frontend/src/Components/shared/searchComponent.vue
@@ -1,28 +1,38 @@
 <template>
   <div class="Search_Container">
     <img class="Search_Icon" src="../../Assets/icons/searchIcon.svg" alt="search" />
-    <input class="Search_Input" type="text" placeholder="Search" :value="search" @keyup="searchInput($event)" />
+    <input class="Search_Input" type="text" placeholder="Search" :value="search" @keyup="searchInput($event)" @focus="this.dropdownActivated = true"/>
+
+    <div class="Search_Dropdown_container" v-if="this.search.length >= 1 && this.dropdownActivated" >
+      <dropdownsearchComponent v-for="feature in filteredFeatures" :key="feature.id" :feature="feature" />
+    </div>
   </div>
+
+  <div class="SearchBackground" v-if="this.dropdownActivated" @click="this.dropdownActivated = false"></div>
 </template>
 
-<script lang="ts">
-let timeout: any = null;
+<script>
+import dropdownsearchComponent from "./dropdownsearchComponent.vue";
+import { mapState } from "vuex";
 export default {
+  components: {
+    dropdownsearchComponent,
+  },
+  computed: {
+    ...mapState(["features"]),
+    filteredFeatures(){
+      return this.features.allFeatures.filter(feature => feature.title.toLowerCase().includes(this.search.toLowerCase()))
+    }
+  },
   data: () => ({
+    dropdownActivated: false,
     search: new URLSearchParams(window.location.search).get("query") || "",
   }),
   methods: {
-    searchInput: function (event: any) {
+    searchInput: function (event) {
+      this.search = event.target.value;
       if (event.key === "Enter") {
-        this.search = event.target.value;
         window.location.href = '/search?query=' + encodeURIComponent(this.search)
-      } else {
-        clearTimeout(timeout);
-
-        timeout = setTimeout(() => {
-          this.search = event.target.value;
-          window.location.href = '/search?query=' + encodeURIComponent(this.search)
-        }, 1000);
       }
     },
   },
@@ -30,6 +40,21 @@ export default {
 </script>
 
 <style>
+.Search_Dropdown_container{
+  position: absolute;
+  width: 220px;
+  height: 300px;
+  overflow-y: auto;
+  overflow-x: hidden;
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  top: 40px;
+  z-index: 1000;
+
+  border-radius: 4px;
+  border: 2px solid #e0e0e0;
+}
 .Search_Container {
   width: fit-content;
   height: 100%;
@@ -43,6 +68,17 @@ export default {
   justify-content: center;
   align-items: center;
   padding: 0 10px;
+
+  position: relative;
+}
+
+.SearchBackground{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  z-index: 999;
 }
 
 .Search_Icon {
